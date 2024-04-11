5532377242371b243bb4befe45fbdacfc3b41d53
jerodev <jeroen@deviaene.eu>
2024-04-11 15:16:01 +0200

    Update visibility

--- laravel/queue.go
+++ laravel/queue.go
@@ -20,25 +20,28 @@ type QueueJob struct {
 	Timeout  *int
 }
 
+// OnQueue sets the queue name where the job will be dispatched
 func (j *QueueJob) OnQueue(q string) *QueueJob {
 	j.Queue = q
 
 	return j
 }
 
+// WithMaxTries sets the maximum amount of thimes the job will be retried
 func (j *QueueJob) WithMaxTries(t int) *QueueJob {
 	*j.MaxTries = t
 
 	return j
 }
 
+// WithTimeout sets the time the job is allowed to run for
 func (j *QueueJob) WithTimeout(t int) *QueueJob {
 	*j.Timeout = t
 
 	return j
 }
 
-func (j QueueJob) CreateJobPayload() JobPayload {
+func (j QueueJob) createJobPayload() jobPayload {
 	data, _ := php.Serialize(j.Payload)
 
 	jobClassString := "O:" + strconv.Itoa(len(j.JobClass)) + ":\"" + j.JobClass + "\":"
@@ -46,7 +49,7 @@ func (j QueueJob) CreateJobPayload() JobPayload {
 
 	id := uuid.New()
 
-	return JobPayload{
+	return jobPayload{
 		Uuid:          id.String(),
 		DisplayName:   j.JobClass,
 		Job:           "Illuminate\\Queue\\CallQueuedHandler@call",
@@ -56,7 +59,7 @@ func (j QueueJob) CreateJobPayload() JobPayload {
 		Backoff:       false,
 		Timeout:       j.Timeout,
 		RetryUntil:    nil,
-		Data: JobPayloadData{
+		Data: jobPayloadData{
 			CommandName: j.JobClass,
 			Command:     data,
 		},
@@ -87,7 +90,7 @@ type RedisQueueConnection struct {
 
 func (c RedisQueueConnection) Dispatch(job QueueJob) {
 	queueName := strings.ToLower(strings.ReplaceAll(c.laravelAppName, " ", "_")) + "_database_queues:" + job.Queue
-	payload, _ := json.Marshal(job.CreateJobPayload())
+	payload, _ := json.Marshal(job.createJobPayload())
 
 	c.client.RPush(
 		context.Background(),
@@ -109,7 +112,7 @@ func NewRedisQueueClient(laravelAppName string, opts *redis.Options) RedisQueueC
 	}
 }
 
-type JobPayload struct {
+type jobPayload struct {
 	Uuid          string         `json:"uuid"`
 	DisplayName   string         `json:"displayName"`
 	Job           string         `json:"job"`
@@ -119,7 +122,7 @@ type JobPayload struct {
 	Backoff       bool           `json:"backoff"`
 	Timeout       *int           `json:"timeout"`
 	RetryUntil    *int           `json:"retryUntil"`
-	Data          JobPayloadData `json:"data"`
+	Data          jobPayloadData `json:"data"`
 	Id            string         `json:"id"`
 	Attepmts      int            `json:"attempts"`
 	Type          string         `json:"type"`
@@ -128,7 +131,7 @@ type JobPayload struct {
 	PushedAt      string         `json:"pushedAt"`
 }
 
-type JobPayloadData struct {
+type jobPayloadData struct {
 	CommandName string `json:"commandName"`
 	Command     string `json:"command"`
 }
