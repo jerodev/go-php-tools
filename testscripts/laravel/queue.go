54a369b1d1646ab5f6ede2d9025eac462005013d
jerodev <jeroen@deviaene.eu>
2024-04-11 11:10:32 +0200

    Map redis ports

--- testscripts/laravel/queue.go
+++ testscripts/laravel/queue.go
@@ -15,7 +15,7 @@ func main() {
 	})
 
 	conn := laravel.NewRedisQueueClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     "127.0.0.1:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
