976a220cd4e31bb5586bf49c03ca75406e13cfce
jerodev <jeroen@deviaene.eu>
2024-11-09 18:15:24 +0100

    WIP

--- php/unserialize.go
+++ php/unserialize.go
@@ -17,13 +17,21 @@ func Unserialize(input string, dest interface{}) error {
 }
 
 func unserializeWalk(input string, position *int, dest interface{}) error {
-	rDest := reflect.ValueOf(dest)
-	if rDest.Kind() != reflect.Pointer || !rDest.Elem().CanSet() {
-		return ErrMustBeWriteable
+	var rDest reflect.Value
+	switch x := dest.(type) {
+	case reflect.Value:
+		rDest = x
+	default:
+		rDest = reflect.ValueOf(dest)
+		if rDest.Kind() != reflect.Pointer || !rDest.Elem().CanSet() {
+			return ErrMustBeWriteable
+		}
+
+		rDest = rDest.Elem()
 	}
 
 	if input == "N;" {
-		rDest.Elem().SetZero()
+		rDest.SetZero()
 		*position += 3
 		return nil
 	}
@@ -33,9 +41,9 @@ func unserializeWalk(input string, position *int, dest interface{}) error {
 		// TODO: unserialize arrays
 	case "b:":
 		if input[*position+3] == '1' {
-			rDest.Elem().SetBool(true)
+			rDest.SetBool(true)
 		} else {
-			rDest.Elem().SetBool(false)
+			rDest.SetBool(false)
 		}
 		walkUntil(input, position, ';')
 	case "d:":
@@ -43,14 +51,14 @@ func unserializeWalk(input string, position *int, dest interface{}) error {
 		if err != nil {
 			return err
 		}
-		rDest.Elem().SetFloat(value)
+		rDest.SetFloat(value)
 	case "i:":
-		value, err := strconv.Atoi(walkUntil(input, position, ';'))
+		value, err := strconv.ParseInt(walkUntil(input, position, ';'), 10, 64)
 		if err != nil {
 			return err
 		}
-		rDest.Elem().SetInt(int64(value))
-	case "O":
+		rDest.SetInt(value)
+	case "O:":
 		return unserializeStruct(input, position, dest)
 	case "s:":
 		return unserializeString(input, position, rDest)
@@ -64,38 +72,66 @@ func unserializeWalk(input string, position *int, dest interface{}) error {
 	return nil
 }
 
+// unserializeNext finds the next serialized value starting from position and sets the value in destination
+func unserializeNext(input string, position *int, dest reflect.Value) error {
+	start := *position
+
+	// For simple values, unserialize until the next ;
+	if input[*position] != 'O' && input[*position] != 'a' {
+		for {
+			*position++
+			if input[*position] == ';' {
+				break
+			}
+		}
+
+		*position++
+
+		return Unserialize(input[start:*position], dest)
+	}
+
+	// TODO: structs and arrays
+
+	return nil
+}
+
 func unserializeString(input string, position *int, dest reflect.Value) error {
 	*position += 3
 	walkUntil(input, position, ':')
 	value := walkUntil(input, position, ';')
-	dest.Elem().SetString(value[1 : len(value)-1])
+	dest.SetString(value[1 : len(value)-1])
+
+	*position++
+
+	return nil
 }
 
 func unserializeStruct(input string, position *int, dest interface{}) error {
-	r := reflect.ValueOf(dest)
-	if r.Elem().Kind() != reflect.Struct {
-		return errors.New("Expected struct destination, got " + r.Elem().Kind().String())
+	r := reflect.ValueOf(dest).Elem()
+	if r.Kind() != reflect.Struct {
+		return errors.New("Expected struct destination, got " + r.Kind().String())
 	}
 
 	walkUntil(input, position, '{')
 	*position++
 
-	fields := map[string]reflect.Value{}
-	for i := range r.Elem().NumField() {
-		field := r.Elem().Field(i)
-		fields[field.Elem().Type().Name()] = field.Elem()
-	}
-
+	var rv reflect.Value
 	fieldName := ""
-	rFieldName := reflect.ValueOf(fieldName)
+	rFieldName := reflect.ValueOf(&fieldName)
 	for {
 		if input[*position] == '}' {
 			break
 		}
 
+		// Find a field name that is part of the struct
 		unserializeString(input, position, rFieldName)
+		rv = r.FieldByName(fieldName)
+		if !rv.IsValid() || !rv.CanSet() {
+			return errors.New("Cannot set field " + fieldName)
+		}
 
-		// TODO: map struct field
+		// Unserialize the value
+		unserializeNext(input, position, rv)
 	}
 
 	*position++
