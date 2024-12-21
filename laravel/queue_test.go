4b7c9c96568d4a7de5bf3d1a4c65ab7d778d119e
jerodev <jeroen@deviaene.eu>
2024-12-21 15:29:49 +0100

    Fix tests

--- laravel/queue_test.go
+++ laravel/queue_test.go
@@ -16,19 +16,19 @@ func TestSerializeJob(t *testing.T) {
 	})
 
 	commandPayload := job.createJobPayload().Data.Command
-	if strings.HasSuffix(commandPayload, `O:27:"App\Events\UpdateDashboards":1:{`) {
-		t.Error("Invalid serialized data: ", commandPayload)
+	if !strings.HasPrefix(commandPayload, `O:27:"App\Events\UpdateDashboards":1:{`) {
+		t.Errorf("Invalid serialized data: `%s`", commandPayload)
 	}
 }
 
 func TestNewBroadcastEvent(t *testing.T) {
-	job, _ := NewQueueJob(`App\Events\UpdateDashboards`, job{
+	job, _ := NewBroadcastEvent(`App\Events\UpdateDashboards`, job{
 		Parts: []string{"foo", "bar"},
 	})
 
 	commandPayload := job.createJobPayload().Data.Command
 	expected := fmt.Sprintf(`O:%v:"%s":1:{s:5:"Event";O:%v:"%s"`, len(broadcastJobClass), broadcastJobClass, len(`App\Events\UpdateDashboards`), `App\Events\UpdateDashboards`)
-	if strings.HasSuffix(commandPayload, expected) {
-		t.Error("Invalid serialized data: ", commandPayload)
+	if !strings.HasPrefix(commandPayload, expected) {
+		t.Errorf("Invalid serialized data `%s`", commandPayload)
 	}
 }
